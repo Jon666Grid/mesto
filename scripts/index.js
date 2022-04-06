76f67068c9ef50b205882f62e8c000d264d5f039
Jon666Grid <jon666gridin@yandex.ru>
2022-04-06 13:29:28 +0700

    Проектная работа:4 Место

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,35 @@
-const profoleButtonActive = document.querySelector ('.profile__button');
-const modalWindow = document.querySelector ('.popup');
-const modalCloseBtn = modalWindow.querySelector ('.popup__close');
-const formSubmitBtn = modalWindow.querySelector ('.form__submit-btn');
+const profoleButtonActive = document.querySelector('.profile__button');
+const modalWindow = document.querySelector('.popup');
+const modalCloseBtn = modalWindow.querySelector('.popup__close');
+const formSubmitBtn = modalWindow.querySelector('.form__submit-btn');
 
 function toggleModalWindow() {
-    modalWindow.classList.toggle ('popup__is-active');
+    modalWindow.classList.toggle('popup_is_opened');
 }
 
-profoleButtonActive.addEventListener ('click', toggleModalWindow);
-modalCloseBtn.addEventListener ('click', toggleModalWindow);
-formSubmitBtn.addEventListener ('click', toggleModalWindow);
-
+profoleButtonActive.addEventListener('click', toggleModalWindow);
+modalCloseBtn.addEventListener('click', toggleModalWindow);
 
 function onOverClick(event) {
     if (event.target === event.currentTarget) {
-    toggleModalWindow();
+        toggleModalWindow();
     }
 }
 
-modalWindow.addEventListener ('click', onOverClick);
+modalWindow.addEventListener('click', onOverClick);
 
+const formElement = document.querySelector('.form');
+const nameInput = formElement.querySelector('.form__text_name_user');
+const jobInput = formElement.querySelector('.form__text_profession_user');
+const profileInfoName = document.querySelector('.profile__info-name');
+const profileInfoProfession = document.querySelector('.profile__info-profession');
 
-const formElement = document.querySelector ('.form');
-const nameInput = formElement.querySelector ('.form__text_name');
-const jobInput = formElement.querySelector ('.form__text_profession');
-const profileInfoName = document.querySelector ('.profile__info-name');
-const profileInfoProfession = document.querySelector ('.profile__info-profession');
-
-
-function formSubmitHandler (event) {
+function formSubmitHandler(event) {
     event.preventDefault();
-    
+
     profileInfoName.textContent = nameInput.value;
     profileInfoProfession.textContent = jobInput.value;
-    
+    toggleModalWindow();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
