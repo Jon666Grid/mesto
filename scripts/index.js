1fc8f4a5424e20a60e0e0c002a28f74fadd68f68
Evgeniy Gridin <jon666gridin@yandex.ru>
2022-04-14 23:28:18 +0700

    ПР4 Место:V2Beta

--- scripts/index.js
+++ scripts/index.js
@@ -36,7 +36,6 @@ const modalWindowTwo = document.querySelector('.popup_type_new-card');
 const modalTwoCloseBtn = modalWindowTwo.querySelector('.popup__close');
 const formTwoSubmitBtn = modalWindowTwo.querySelector('.form__submit-btn');
 
-
 profoleButtonActive.addEventListener('click', () => togglePopup(modalWindow));
 modalCloseBtn.addEventListener('click', () => togglePopup(modalWindow));
 
@@ -47,42 +46,37 @@ function togglePopup(popup) {
     popup.classList.toggle('popup_is_opened');
 }
 
+
 function onOverlayClick(event) {
     if (event.target === event.currentTarget) {
         modalWindow.classList.remove('popup_is_opened');
         modalWindowTwo.classList.remove('popup_is_opened');
     }
 }
+
 modalWindow.addEventListener('click', onOverlayClick);
 modalWindowTwo.addEventListener('click', onOverlayClick);
 
 
-const formElement = document.querySelector('.form');
-
-const nameInput = formElement.querySelector('.form__text_type_name');
-const jobInput = formElement.querySelector('.form__text_type_profession');
+const formTypeEdit = document.querySelector('.forum_type_edit');
+const nameInput = formTypeEdit.querySelector('.form__text_type_name');
+const jobInput = formTypeEdit.querySelector('.form__text_type_profession');
 const profileInfoName = document.querySelector('.profile__info-name');
 const profileInfoProfession = document.querySelector('.profile__info-profession');
 
-const areaInput = formElement.querySelector('.form__text_type_area');
-const imgInput = formElement.querySelector('.form__text_type_img');
-const areaTitleCard = document.querySelector('.popup__title');
-const imgUrlCard = document.querySelector('.popup__img');
-
 function formSubmitHandler(event) {
     event.preventDefault();
-
     profileInfoName.textContent = nameInput.value;
     profileInfoProfession.textContent = jobInput.value;
-    togglePopup(modalWindow);
+    togglePopup(modalWindow); 
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-
+formTypeEdit.addEventListener('submit', formSubmitHandler);
 
 
 const listElements = document.querySelector('.elements__list');
 const cardTemplateAdd = document.querySelector('.card-template');
+const CardDelButton = document.querySelector ('.card__del-button')
 
 
 function render() {
@@ -103,3 +97,8 @@ function getCard(item) {
 }
 
 render();
+
+// function deliteCard (event) {
+//     const DelButton = event.target.listElements;
+//     DelButton.remove();
+// }
