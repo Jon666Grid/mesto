38ff70e2bf619035f949015343376cb4c027152a
Jon666Grid <jon666gridin@yandex.ru>
2022-04-14 17:07:48 +0700

    ПР4 Место:V2Beta

--- scripts/index.js
+++ scripts/index.js
@@ -25,7 +25,6 @@ const initialCards = [
     }
 ];
 
-const popup = document.querySelector('.popup');
 
 const profoleButtonActive = document.querySelector('.profile__button');
 const modalWindow = document.querySelector('.popup_type_edit');
@@ -37,45 +36,70 @@ const modalWindowTwo = document.querySelector('.popup_type_new-card');
 const modalTwoCloseBtn = modalWindowTwo.querySelector('.popup__close');
 const formTwoSubmitBtn = modalWindowTwo.querySelector('.form__submit-btn');
 
-profoleButtonActive.addEventListener('click', ()=>togglePopup(0));
-// modalCloseBtn.addEventListener('click', ()=>togglePopup(modalWindow));
 
-cardButtonActive.addEventListener('click', ()=>togglePopup(1));
+profoleButtonActive.addEventListener('click', () => togglePopup(modalWindow));
+modalCloseBtn.addEventListener('click', () => togglePopup(modalWindow));
 
+cardButtonActive.addEventListener('click', () => togglePopup(modalWindowTwo));
+modalTwoCloseBtn.addEventListener('click', () => togglePopup(modalWindowTwo));
 
-
-function togglePopup () {
-    popup[0].classList.toggle('popup_is_opened')
+function togglePopup(popup) {
+    popup.classList.toggle('popup_is_opened');
 }
 
-// function closePopup () {
-//     popup.classList.remove('popup_is_opened')
-// }
+function onOverlayClick(event) {
+    if (event.target === event.currentTarget) {
+        modalWindow.classList.remove('popup_is_opened');
+        modalWindowTwo.classList.remove('popup_is_opened');
+    }
+}
+modalWindow.addEventListener('click', onOverlayClick);
+modalWindowTwo.addEventListener('click', onOverlayClick);
+
+
+const formElement = document.querySelector('.form');
+
+const nameInput = formElement.querySelector('.form__text_type_name');
+const jobInput = formElement.querySelector('.form__text_type_profession');
+const profileInfoName = document.querySelector('.profile__info-name');
+const profileInfoProfession = document.querySelector('.profile__info-profession');
+
+const areaInput = formElement.querySelector('.form__text_type_area');
+const imgInput = formElement.querySelector('.form__text_type_img');
+const areaTitleCard = document.querySelector('.popup__title');
+const imgUrlCard = document.querySelector('.popup__img');
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+
+    profileInfoName.textContent = nameInput.value;
+    profileInfoProfession.textContent = jobInput.value;
+    togglePopup(modalWindow);
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
 
 
 
+const listElements = document.querySelector('.elements__list');
+const cardTemplateAdd = document.querySelector('.card-template');
 
 
-// function onOverClick(event) {
-//     if (event.target === event.currentTarget) {
-//         popup.classList.remove('popup_is_opened');
-//     }
-// }
+function render() {
+    const html = initialCards.map(getCard);
+    listElements.append(...html);
+}
 
-// popup.addEventListener('click', onOverClick);
+function getCard(item) {
+    const getTemplateList = cardTemplateAdd.content.cloneNode(true);
 
-// const formElement = document.querySelector('.form');
-// const nameInput = formElement.querySelector('.form__text_type_name');
-// const jobInput = formElement.querySelector('.form__text_type_profession');
-// const profileInfoName = document.querySelector('.profile__info-name');
-// const profileInfoProfession = document.querySelector('.profile__info-profession');
+    const titleCard = getTemplateList.querySelector('.card__title');
+    titleCard.textContent = item.name;
 
-// function formSubmitHandler(event) {
-//     event.preventDefault();
+    const ImageCard = getTemplateList.querySelector('.card__img');
+    ImageCard.src = item.link;
 
-//     profileInfoName.textContent = nameInput.value;
-//     profileInfoProfession.textContent = jobInput.value;
-//     toggleModalWindow();
-// }
+    return getTemplateList;
+}
 
-// formElement.addEventListener('submit', formSubmitHandler);
+render();
