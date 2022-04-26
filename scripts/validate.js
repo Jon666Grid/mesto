a8ad7fe5eedcff7046d6c56225a267598cb6f34e
Evgeniy Gridin <jon666gridin@yandex.ru>
2022-04-26 23:11:52 +0700

    ПР6 Место:V2

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,26 +1,47 @@
 
 
-// function enableValidation(formSelector) {
-//     const form = document.querySelector(formSelector);
+function enableValidation(config) {
+   const form = document.querySelector(config.formSelector);
+   const inputs = form.querySelectorAll(config.inputSelector);
+   // const errorClass = form.querySelectorAll(config.inputErrorClass);
 
-//     form.addEventListener('submit', (event) => handleSubmit(event));
+   inputs.forEach((element) => {
+      element.addEventListener('input', handleFormInput);
+   });
+
+   form.addEventListener('submit', (event) => handleSubmit(event, form));
+}
+
+
+// function handleSubmit(event, form) {
+//    event.preventDefault();
+
+//    if (form.checkValidity()) {
+//       alert('Валидна');
+//    } else {
+//       alert('Не волидна');
+//    }
+
 // }
 
+function handleFormInput(event) {
+   const input = event.target;
+   const errorNode = document.querySelector(`#${input.id}-error`);
 
-// function handleSubmit(event) {
-//     event.preventDefault();
+   if (input.validity.valid) {
+      errorNode.textContent ='';
+      input.classList.remove('popup__input_type_error');
+   } else {
+      errorNode.textContent = input.validationMessage;
+      input.classList.add('popup__input_type_error');
+   }
+}
 
-//     const form = event.currentTarget;
-
-// }
-
-// enableValidation({
-//     formSelector: '.popup__form',
-//     inputSelector: '.popup__input',
-//     submitButtonSelector: '.popup__button',
-//     inactiveButtonClass: 'popup__button_disabled',
-//     inputErrorClass: 'popup__input_type_error',
-//     errorClass: 'popup__error_visible'
-//   }); 
-
-// enableValidation('.form');
+enableValidation({
+   formSelector: '.popup__form',
+   inputSelector: '.popup__input',
+   // submitButtonSelector: '.popup__button',
+   // inactiveButtonClass: 'popup__button_disabled',
+   inputErrorClass: 'popup__input_type_error',
+   // errorClass: 'popup__error_visible'
+}); 
