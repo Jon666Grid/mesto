aa11b3c168e8544bcecdeb6c7bad18eb1d4080c3
Jon666Grid <jon666gridin@yandex.ru>
2022-04-28 15:51:41 +0700

    ПР6 Место:V3

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,51 +1,47 @@
 
-
 function enableValidation(config) {
-   const form = document.querySelector(config.formSelector);
-   const inputs = form.querySelectorAll(config.inputSelector);
-   
+   const form = Array.from(document.querySelectorAll(config.formSelector));
+  form.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    const inputs = Array.from(formElement.querySelectorAll(config.inputSelector));
+    inputs.forEach((inputList) => {inputList.addEventListener('input',(event) => handleFormInput (event, config, formElement));
+  });
+  toggleButton(config, formElement);
+});
+};
 
-   inputs.forEach((element) => {
-      element.addEventListener('input', (event) => handleFormInput (event, form, config));
-   });
-
-   form.addEventListener('submit', (event) => handleSubmit(event, form, config));
-
-   toggleButton(form, config);
-}
-
-function toggleButton(form, config) {
-   const buttonBtn =document.querySelector(config.submitButtonSelector)
-   buttonBtn.disabled = !form.checkValidity();
-
-   buttonBtn.classList.toggle('popup__submit-btn_disabled', !form.checkValidity())
-}
-
-// function handleSubmit(event, form) {
+// function handleSubmit(event, config, formElement) {
 //    event.preventDefault();
 
-//    if (form.checkValidity()) {
+//    if (formElement.checkValidity()) {
 //       alert('Валидна');
 //    } else {
-//       alert('Не волидна');
+//       toggleButton(config, formElement);
 //    }
 
 // }
 
-function handleFormInput(event, form, config) {
+function handleFormInput(event, config, formElement) {
    const input = event.target;
    const errorNode = document.querySelector(`#${input.id}-error`);
 
    if (input.validity.valid) {
       errorNode.textContent ='';
-      input.classList.remove('popup__input_type_error');
+      input.classList.remove(config.inputErrorClass,);
    } else {
-      errorNode.textContent = 'Вы пропустили это поле.';
-      input.classList.add('popup__input_type_error');
+      errorNode.textContent = input.validationMessage;
+      input.classList.add(config.inputErrorClass);
    }
-   toggleButton(form, config);
+   toggleButton(config, formElement);
 }
 
+function toggleButton(config, formElement) {
+   const buttonBtn =formElement.querySelector(config.submitButtonSelector)
+   buttonBtn.disabled = !formElement.checkValidity();
+   buttonBtn.classList.toggle(config.inactiveButtonClass, !formElement.checkValidity());
+}
 
 
 enableValidation({
@@ -54,5 +50,5 @@ enableValidation({
    submitButtonSelector: '.popup__submit-btn',
    inactiveButtonClass: 'popup__submit-btn_disabled',
    inputErrorClass: 'popup__input_type_error',
-   errorClass: 'popup__error_visible'
-}); 
+   errorClass: 'popup__input-error_visible'
+});
