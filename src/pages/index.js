f391acb913e177bdf5176203f071e4c0bdfb68c6
Evgeniy Gridin <jon666gridin@yandex.ru>
2022-06-12 15:51:38 +0700

    Проектная работа 8 :Место

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import '../pages/index.css'
+
 import {
     initialCards,
     config,
@@ -12,13 +14,13 @@ import {
     jobInput,
     name,
     profession
-} from './constants.js';
-import { FormValidator } from './FormValidator.js';
-import Card from './Card.js';
-import Section from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
+} from '../utils/constants.js';
+import { FormValidator } from '../components/FormValidator.js';
+import Card from '../components/Card.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
 
 const profileValidator = new FormValidator(config, modalWindowEdit);
 profileValidator.enableValidation();
@@ -33,7 +35,7 @@ const createCard = (data) => {
         data => popupWithImage.open(data)
     );
     return card.generateCard();
-}
+} 
 
 const section = new Section({
     items: initialCards,
